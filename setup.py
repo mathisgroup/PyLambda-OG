f6a4927bc5290138fcdaf35a7dc829f37a9fe3c4
Cole Mathis <cole.mathis.ool@gmail.com>
2024-02-09 00:53:10 -0700

    More changes

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
+from setuptools.command.install import install as BaseInstall
+
 setup(
     name='PyLambda_OG',
     version='0.0.1',
     install_requires=[
         'python_version>="3.8"',
     ],
+    package_data={ 'PyLambda_OG': ['LambdaC/lambda.so'] },
 )
